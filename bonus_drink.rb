759c93b48739fe0175b2341cce9cee0d76e9fc67
hirotaca <developer.hiro.east9.hey.world@gmail.com>
2016-07-15 03:37:02 +0900

    私の解答。リファクタリング

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -2,12 +2,11 @@ class BonusDrink
 
   def self.total_count_for(amount)
 
-    return 0 if amount.negative?
+    return 0 if amount.negative? || amount.zero?
 
     bonus_bottle = amount.odd? ? amount.div(2) : amount.div(2).pred
-    bonus_bottle = 0 if amount.zero?
 
-    amount + bonus_bottle
+    return amount + bonus_bottle
 
   end
 
